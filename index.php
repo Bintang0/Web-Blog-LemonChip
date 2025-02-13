b40b349d657b12c489bd38845d82cf6d1cedccbe
indritanialestari <indritanialestari@gmail.com>
2025-02-13 15:54:25 +0700

    read more

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ $all = query('SELECT * FROM artikel') ?>
           aria-expanded="false"
           style="border: none; background: transparent;">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style="width: 24px; height: 24px; fill: #ffffff;">
-      <path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464l349.5 0c-8.9-63.3-63.3-112-129-112l-91.4 0c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304l91.4 0C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7L29.7 512C13.3 512 0 498.7 0 482.3z"/>
+      <path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464l349.5 0c-8.9-63.3-63.3-112-129-112l-91.4 0c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304l91.4 0C368.2 304 448 383.8 448 482.3c0 16.4-133 29.7-29.7 29.7L29.7 512C13.3 512 0 498.7 0 482.3z"/>
     </svg>
   </button>
   
@@ -73,7 +73,7 @@ $all = query('SELECT * FROM artikel') ?>
     <li><a class="dropdown-item" href="login.php">Login</a></li>
     <li><hr class="dropdown-divider"></li>
     <li><a class="dropdown-item" href="#">Logout</a></li>
-  </ul>
+  </ul>
     </div>
   </div>
 </nav>
@@ -117,7 +117,7 @@ $all = query('SELECT * FROM artikel') ?>
                 <h5 class="card-title"><?= $alls["judul"] ;?></h5>
                 <h6 class="card-subtitle mb-2 text-body-secondary"><?= $alls["tanggal"] ;?></h6>
                 <p class="card-text"><?= $alls["isi"] ;?></p>
-                <a class="btn btn-primary" role="button" aria-disabled="true">Read More</a>
+                <a href="detail.php?id=<?= $alls['id']; ?>" class="btn btn-primary" role="button" aria-disabled="true">Read More</a>
             </div>
         </div>
         <?php } ?>
